59895287a5f99319b8deb197136ab8692fbe6da1
tsurumeso <nstm101339@gmail.com>
2017-02-08 14:19:52 +0900

    Use double precision

--- pysparcl/internal.pyx
+++ pysparcl/internal.pyx
@@ -1,38 +1,43 @@
+from __future__ import division
+
 import six
+import cython
 import numpy as np
 cimport numpy as np
-from libc.math cimport fabs
+from libc.math cimport abs
 
-FLOAT = np.float32
-ctypedef np.float32_t FLOAT_t
+DOUBLE = np.float64
+ctypedef np.float64_t DOUBLE_t
 
-
-def distfun(np.ndarray[FLOAT_t, ndim=2] x):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def distfun(np.ndarray[DOUBLE_t, ndim=2] x):
     cdef int i = 0
     cdef int j = 0
     cdef int ip = 0
     cdef int ii = 0
     cdef int n = x.shape[0]
     cdef int p = x.shape[1]
-    cdef int n2 = <int>(n * (n - 1) / 2.)
-    cdef np.ndarray[FLOAT_t, ndim = 2] d = np.zeros((n2, p), dtype=FLOAT)
+    cdef int n2 = n * (n - 1) // 2
+    cdef np.ndarray[DOUBLE_t, ndim = 2] d = np.zeros((n2, p), dtype=DOUBLE)
     for i in six.moves.range(n):
         for ip in six.moves.range(i + 1, n):
             for j in six.moves.range(p):
-                d[ii, j] = fabs(x[i, j] - x[ip, j])
+                d[ii, j] = abs(x[i, j] - x[ip, j])
             ii += 1
     return d
 
-
-def multfun(np.ndarray[FLOAT_t, ndim=2] x):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def multfun(np.ndarray[DOUBLE_t, ndim=2] x):
     cdef int i = 0
     cdef int j = 0
     cdef int ip = 0
     cdef int ii = 0
     cdef int n = x.shape[0]
     cdef int p = x.shape[1]
-    cdef int n2 = <int>(n * (n - 1) / 2.)
-    cdef np.ndarray[FLOAT_t, ndim = 2] d = np.zeros((n2, p), dtype=FLOAT)
+    cdef int n2 = n * (n - 1) // 2
+    cdef np.ndarray[DOUBLE_t, ndim = 2] d = np.zeros((n2, p), dtype=DOUBLE)
     for i in six.moves.range(n):
         for ip in six.moves.range(i + 1, n):
             for j in six.moves.range(p):
