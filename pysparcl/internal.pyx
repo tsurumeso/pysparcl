95721dc0214747a02d24666ff66191857e712929
tsurumeso <nstm101339@gmail.com>
2017-02-19 02:23:03 +0900

    Simplify code

--- pysparcl/internal.pyx
+++ pysparcl/internal.pyx
@@ -1,46 +1,46 @@
 from __future__ import division
 
-import six
 import cython
-import numpy as np
-cimport numpy as np
+
+import numpy
+cimport numpy
+
 from libc.math cimport abs
 
-DOUBLE = np.float64
-ctypedef np.float64_t DOUBLE_t
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def distfun(np.ndarray[DOUBLE_t, ndim=2] x):
-    cdef int i = 0
-    cdef int j = 0
-    cdef int ip = 0
+def distfun(double[:, :] x):
+    cdef int i, j, ip
     cdef int ii = 0
     cdef int n = x.shape[0]
     cdef int p = x.shape[1]
     cdef int n2 = n * (n - 1) // 2
-    cdef np.ndarray[DOUBLE_t, ndim = 2] d = np.zeros((n2, p), dtype=DOUBLE)
-    for i in six.moves.range(n):
-        for ip in six.moves.range(i + 1, n):
-            for j in six.moves.range(p):
+    cdef double[:, :] d = numpy.zeros((n2, p), dtype=numpy.double)
+
+    for i in range(n):
+        for ip in range(i + 1, n):
+            for j in range(p):
                 d[ii, j] = abs(x[i, j] - x[ip, j])
             ii += 1
-    return d
+
+    return numpy.array(d)
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def multfun(np.ndarray[DOUBLE_t, ndim=2] x):
-    cdef int i = 0
-    cdef int j = 0
-    cdef int ip = 0
+def multfun(double[:, :] x):
+    cdef int i, j, ip
     cdef int ii = 0
     cdef int n = x.shape[0]
     cdef int p = x.shape[1]
     cdef int n2 = n * (n - 1) // 2
-    cdef np.ndarray[DOUBLE_t, ndim = 2] d = np.zeros((n2, p), dtype=DOUBLE)
-    for i in six.moves.range(n):
-        for ip in six.moves.range(i + 1, n):
-            for j in six.moves.range(p):
+    cdef double[:, :] d = numpy.zeros((n2, p), dtype=numpy.double)
+
+    for i in range(n):
+        for ip in range(i + 1, n):
+            for j in range(p):
                 d[ii, j] = x[i, j] * x[ip, j]
             ii += 1
-    return d
+
+    return numpy.array(d)
